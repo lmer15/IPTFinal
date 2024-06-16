9ce35c38d7537450a0e414caaf8db7488307363b
lmer15 <raponelmer15@gmail.com>
2024-06-16 21:36:13 +0800

    FINAL OUTPUT

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="homepage.php">Home</a>
+              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="login.php">Accounts</a>
